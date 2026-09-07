807f48ea03ad65ebe852ccde7324201c7c126b88
Sathwik1303 <sathwikreddyd13@gmail.com>
2026-09-07 20:06:31 +0530

    LEETCODE QUESTION #283

--- 283.py
+++ 283.py
@@ -1,4 +1,6 @@
 #THIS IS #283 QUESTION IN LEETCODE.
+#I GOT TO KNOW THAT I AM NOT READING THE QUESTION PROPERLY BECAUSE
+#I USED SORT FUNCTION BEFORE WHICH WAS NOT NEEDED.
 
 class Solution(object):
     def moveZeroes(self, nums):
@@ -7,4 +9,4 @@ class Solution(object):
             if nums[fast]!=0:
                 nums[slow],nums[fast]=nums[fast],nums[slow]
                 slow+=1
-        return nums 
+        return nums
