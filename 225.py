316efe4d961f30695230b234906435459ceb8bd7
Sathwik1303 <sathwikreddyd13@gmail.com>
2026-09-20 20:48:58 +0530

    LEETCODE QUESTION 225

---
+++ 225.py
@@ -0,0 +1,30 @@
+#IMPLENTING STACKS USING QUEUE
+
+from collections import deque
+
+class MyStack:
+
+    def __init__(self):
+        self.q = deque()
+
+    def push(self, x):
+        self.q.append(x)                     
+        for _ in range(len(self.q) - 1):      
+            self.q.append(self.q.popleft())   
+        
+
+    def pop(self):
+        return self.q.popleft() if self.q else None
+
+    def top(self):
+        return self.q[0] if self.q else None
+
+    def empty(self):
+        return len(self.q) == 0
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty() 
