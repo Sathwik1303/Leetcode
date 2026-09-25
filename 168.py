02cbfb03d5d31a4d266f5388d5870c92a324c584
Sathwik1303 <sathwikreddyd13@gmail.com>
2026-09-25 22:12:10 +0530

    LEETCODE QUESTION 168

---
+++ 168.py
@@ -0,0 +1,11 @@
+#EXCEL SHEET COLUMN TITLE
+
+class Solution(object):
+    def convertToTitle(self, columnNumber):
+        result = ""
+        while columnNumber > 0:
+            columnNumber -= 1                     
+            remainder = columnNumber % 26          
+            result = chr(65 + remainder) + result   
+            columnNumber //= 26
+        return result
